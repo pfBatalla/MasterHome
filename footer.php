ff6ab33ab3b966f55d51b6d20268ebaa98333c41
marcgaya98 <34577133+marcgaya98@users.noreply.github.com>
2017-12-19 14:51:11 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html>
-<html>
 <head>
-	
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="CSS/footer.css">
 </head>
-	<?php
-	function pieDeWeb(){
-		?>
-		
-	}
-	?>
-
-
-</html>
+<?php
+function pie(){
+    ?>
+	<footer>
+		<div id="social">
+			<a href="#" title="facebook" class="icon"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+			<a href="#" title="twitter" class="icon"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+			<a href="#" title="google" class="icon"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+			<a href="#" title="instagram" class="icon"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+			<a href="#" title="skype" class="icon"><i class="fa fa-skype" aria-hidden="true"></i></a>
+			<a href="#" title="whatsapp" class="icon"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+			<a href="#" title="youtube" class="icon"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+		</div>
+		<div id="footer-text">
+			<strong><p id="copyright">Copyright</p></strong>
+			<p>Init DAW &copy; 2017 <a href="#">Privacy Policy</a></p>
+		</div>
+	<footer>
+<?php
+}
+?>
